92b38d61e3f364401164995f8ae3719fc09544b9
ghostfacedlabs <ghostfacedlabs@gmail.com>
2021-07-21 12:39:10 -0700

    application complete

--- src/com/ghostfacedlabs/Main.java
+++ src/com/ghostfacedlabs/Main.java
@@ -1,10 +1,44 @@
 package com.ghostfacedlabs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, World!");
-        System.out.println("Test committing");
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This application will give you PI to the Nth decimal\nUp to 15 decimal places!");
+        System.out.println("------------------------------------");
+
+        while (true)  {
+            System.out.println();
+            System.out.print("Please provide a number: ");
+            String digits = scanner.nextLine();
+            int number;
+
+            try {
+                number = Integer.parseInt(digits);
+            } catch (NumberFormatException n) {
+                System.out.println("Must be a valid number");
+                continue;
+            }
+
+            if (number > 0 && number < 16) {
+                String pi = findPiDigits(number);
+                System.out.println("PI to the " + number + "th digit is: " + pi);
+                break;
+            } else {
+                System.out.println("Must be a number 15 or under");
+            }
+        }
+
+    }
+
+    public static String findPiDigits(int number) {
+        double pi = Math.PI;
+        String piString = "" + pi;
+
+
+        return piString.substring(0, number + 2);
     }
 }
